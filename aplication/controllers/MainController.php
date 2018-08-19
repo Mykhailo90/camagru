d9d2483f8a9a88bf07ac602081e58f3b229e8080
Mykhailo SARAPII <msarapii@e1r1p17.unit.ua>
2018-08-19 13:22:48 +0300

    new banner

--- aplication/controllers/MainController.php
+++ aplication/controllers/MainController.php
@@ -9,28 +9,12 @@ class MainController extends Controller{
 
   function __construct($parameters){
 		parent::__construct($parameters);
-
-		$this->params = $parameters;
 		$this->view = new View($parameters);
 	 }
 
     function listAction(){
 			$title = 'DTEK Academy';
       $data = $this->model->get_data();
-
-      // $db = new Db;
-      // $form = '1; Delete FROM test';
-      // $params = ['id'=>$form,];
-      //
-      // // $data = $db->column('SELECT name FROM test WHERE id = :id', $params);
-      //
-      // $prop = $this->model->get_data();
-      //
-
-
-      // debug($vars);
-
-    
       $this->view->render($title, $data);
     }
   }
